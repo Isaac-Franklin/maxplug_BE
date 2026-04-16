4c15657f9e4ecefe87a84e5d8a50a2d968704465
Franklin Isaac <praisefranklin005@gmail.com>
2026-04-16 23:59:13 +0100

    update

--- maxplug_be/settings.py
+++ maxplug_be/settings.py
@@ -66,6 +66,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'userID.apps.UseridConfig',
+    'storeapp.apps.StoreappConfig',
     
     # 
     'rest_framework',
